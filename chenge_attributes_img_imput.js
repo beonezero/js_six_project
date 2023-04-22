49a403d83214b215df679c4811c71b71001b53a1
Yauheni Neakhai <97246653+beonezero@users.noreply.github.com>
2023-04-22 10:31:24 +0300

    Create chenge_attributes_img_imput.js

---
+++ chenge_attributes_img_imput.js
@@ -0,0 +1,11 @@
+const img = document.querySelector("#logo");
+
+img.setAttribute("src", "/img/php.png");
+// img.setAttribute("width", "100");
+img.src = "/img/js.png";
+img.width = "200";
+
+const button = document.querySelector("#button");
+
+button.value = "3";
+button.type = "number";
