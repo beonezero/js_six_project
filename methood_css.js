ab63396062906c8c1c027fcbb3b96629b9f49778
Yauheni Neakhai <97246653+beonezero@users.noreply.github.com>
2023-04-22 10:03:07 +0300

    Create methood_css.js

---
+++ methood_css.js
@@ -0,0 +1,8 @@
+const heading = document.querySelector("h2");
+console.log(heading);
+// heading.classList.
+heading.classList.add("red-text");
+heading.classList.remove("red-text");
+
+heading.classList.toggle("green-text");
+heading.classList.toggle("green");
